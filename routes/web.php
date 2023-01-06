1d810625e7094727fcca973cb7665e357f14b653
raul llanos <r.llanosrod@gmail.com>
2023-01-06 15:11:57 -0500

    cambio final v3

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::redirect('/','login');
+
 
 Route::get('report', [ReportController::class, 'report'])->name('report');
 Route::get('reportMueble', [ReportController::class, 'reportMueble'])->name('reportMueble');
